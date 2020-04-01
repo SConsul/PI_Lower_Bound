46f71b7794986bef8a692922da5d6c7d7d70723f
SConsul <sarthakconsul@gmail.com>
2020-04-01 10:24:05 +0530

    fixing float error

--- code/solve_adv_memoryless_sir.py
+++ code/solve_adv_memoryless_sir.py
@@ -38,8 +38,8 @@ def solve_adv(num_states, num_actions, reward_function, transition_function, dis
         y = np.dot(pow_arr,pol_y)
         # print("Time",num_iterations,"Current:", np.flip(policy[::2][:-1],axis=0)," | ",np.flip(policy[1::2],axis=0), "Next: ",np.flip(next_policy[::2][:-1],axis=0)," | ", np.flip(next_policy[1::2],axis=0))
         
-        # print("Time",num_iterations,np.flip(next_policy[::2][:-1],axis=0)," | ", np.flip(next_policy[1::2],axis=0),"x=",x,"y=",y)
-        print("Time",num_iterations,next_policy,"x=",x,"y=",y)
+        print("Time",num_iterations,np.flip(next_policy[::2][:-1],axis=0)," | ", np.flip(next_policy[1::2],axis=0),"x=",x,"y=",y)
+        # print("Time",num_iterations,next_policy,"x=",x,"y=",y)
         
         policy = next_policy
     
@@ -70,9 +70,9 @@ def get_next_IS(curr_policy, num_actions,done_flag):
         done_flag = True  
         return 0, True
     else:
-        d = y-x
+        d = round(y-x,8)
         if d==0:
-            print('t1')
+            # print('t1')
             for i in range(S):
                 if pol_x[i] != num_actions-1:
                     break
@@ -80,12 +80,12 @@ def get_next_IS(curr_policy, num_actions,done_flag):
         else:
             L = math.floor(round(math.log(d,num_actions),8))
         if d==1:
-            print('t2')
+            # print('t2')
             return 0, False
         elif pol_y[0]==num_actions-1:
-            print('t3')
-            print(L)
+            # print('t3')
+            # print(L)
             return int(2*L-1), False
         else:
-            print('t4')
+            # print('t4')
             return int(2*L), False
